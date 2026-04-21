3c6db5646575b3b4835ef606d8b7b79da9d59702
Pascal Pomper <git@mail.pasi.dev>
2026-04-21 09:07:20 +0200

    Add MCP server

---
+++ src/HEAL.HeuristicAgent.Mcp.Server/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using HEAL.HeuristicLibAdapter;
+using HEAL.HeuristicLibAdapter.Grpc;
+using HEAL.HeuristicLibWrapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration.Sources.Clear();
+builder.Configuration
+    .AddJsonFile("appsettings.json");
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole(opt =>
+    opt.LogToStandardErrorThreshold = LogLevel.Trace
+);
+
+if (!Enum.TryParse<ClientType>(builder.Configuration["ClientType"], true, out var clientType))
+{
+    throw new InvalidOperationException(
+        "Invalid ClientType configuration. " +
+        $"ClientType must be one of: {string.Join(", ", Enum.GetNames<ClientType>())}."
+    );
+}
+
+var serverUrl = builder.Configuration["Server"];
+
+builder.Services
+    .AddSingleton<IHeuristicLibClient>(
+        clientType switch
+        {
+            ClientType.Grpc => new HeuristicLibGrpcClient(
+                serverUrl ?? throw new InvalidOperationException("Server URL must be provided in configuration.")
+            ),
+            ClientType.Lib => new HeuristicLibClient(),
+            _ => throw new UnreachableException(),
+        }
+    );
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+await builder.Build().RunAsync();
+
+internal enum ClientType
+{
+    Lib,
+    Grpc,
+}
---
+++ src/HEAL.HeuristicAgent.Mcp.Server/Tools/HeuristicTools.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using HEAL.HeuristicLibAdapter;
+using HEAL.HeuristicLibContracts.Dtos;
+using ModelContextProtocol.Server;
+
+namespace HEAL.HeuristicAgent.Mcp.Server.Tools;
+
+[McpServerToolType]
+public sealed class HeuristicTools(IHeuristicLibClient client)
+{
+    [McpServerTool, Description("Runs a symbolic regression algorithm with the given hyperparameters and data.")]
+    public async Task<string> RunSymRegAsync(SymRegProblemDto parameters, CancellationToken ct = default)
+        => await client.RunSymRegAsync(parameters, ct);
+}
